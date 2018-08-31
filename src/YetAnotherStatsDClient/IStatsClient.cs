ae45bb1c3f222705566ecb9a00f7f7b6573e415d
Mike Goodfellow <mike.goodfellow@just-eat.com>
2018-08-31 15:24:31 +0100

    CHANGED: Allow gauges to be set or changes

--- src/YetAnotherStatsDClient/IStatsClient.cs
+++ src/YetAnotherStatsDClient/IStatsClient.cs
@@ -2,9 +2,33 @@
 {
     public interface IStatsClient
     {
+        /// <summary>
+        /// Send a count to the stats server
+        /// </summary>
+        /// <param name="metric"></param>
+        /// <param name="value"></param>
         void Count(string metric, int value = 1);
-        void Gauge(string metric, double value);
+
+        /// <summary>
+        /// Send a gauge value to the stats server
+        /// </summary>
+        /// <param name="metric"></param>
+        /// <param name="value"></param>
+        /// <param name="changeValue">If true, change the value (+/- prefix), otherwise, set the value</param>
+        void Gauge(string metric, double value, bool changeValue = false);
+
+        /// <summary>
+        /// Send timing information to the stats server
+        /// </summary>
+        /// <param name="metric"></param>
+        /// <param name="value"></param>
         void Timing(string metric, int value);
+
+        /// <summary>
+        /// Send timing information to the stats server
+        /// </summary>
+        /// <param name="metric"></param>
+        /// <param name="value"></param>
         void Timing(string metric, long value);
     }
 }
